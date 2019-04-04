939f8d1bf1a80b611b7c4c9e623ec47da99d5770
Nathaniel Holeksa <Nathaniel@Nathaniels-MacBook-Pro.local>
2019-04-04 15:01:53 -0400

    added the thicc CLI

---
+++ thiccc.py
@@ -0,0 +1,81 @@
+import requests 
+import json
+import optparse
+import sys
+
+host = 'http://localhost:7133/services/'
+args = sys.argv
+length = leng(args)
+
+
+if length < 2:
+    badInput()
+
+command = args[1]
+
+if command = "?":
+    print("Command options include ..... .... ....")
+
+elif command = "create":
+    #check right # args
+    if length < 3:
+        badInput()
+
+    serviceName = args[2]
+    containerCommand = ' '.join(args[3:])
+
+    createService(serviceName, containerCommand)
+
+elif command = "delete":
+    #check right # args
+    if length != 3:
+        badInput()
+    
+    serviceName = args[2]
+
+    deleteService(serviceName)
+
+
+elif command = "scale":
+    #check right # args
+    if length != 4:
+        badInput()
+
+    serviceName = args[2]
+    numWorkers = args[3]
+
+    scaleService(serviceName, numWorkers)
+
+
+    #create service 
+def createService(serviceName, command):
+    payload = {"command":command}
+    r = requests.put(host + serviceName, payload)
+
+    print(r.json)
+
+
+    #scale service
+def scaleService(serviceName, numberOfWorkers):
+    num = 0
+    try:
+        num = int(numberOfWorkers)
+    except e:
+        print("Error: Expected an integer for number of workers, command failed")
+        exit(1)
+
+    payload = {"number":num}
+    r = requests.post(host + serviceName, payload)
+
+    print(r.json)
+
+
+    #delete service
+def deleteService(serviceName):
+    r = requests.delete(host + serviceName)
+    print(r.json)
+
+#if we get unexpected input
+def badInput():
+    print("Error: enter a valid command. Execute \'thiccc ?\' for help")
+    exit(1)
